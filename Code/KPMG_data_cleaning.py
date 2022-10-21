5212c61d91dcbf70b84a89de5513414a557d5a1c
Jossie <qingyuan3074@gmail.com>
2022-10-21 10:37:48 +0800

    upload excel

--- Code/KPMG_data_cleaning.py
+++ Code/KPMG_data_cleaning.py
@@ -1,5 +1,19 @@
 import pandas as pd
+import openpyxl
 import matplotlib as plot
-import sklearn
+path = "..\\Data\\KPMG_VI_New_raw_data_update_final.xlsx"
+NewCustomerList = pd.read_excel(path, sheet_name="NewCustomerList")
+Transactions = pd.read_excel(path, sheet_name="Transactions")
+CustomerDemographic = pd.read_excel(path, sheet_name="CustomerDemographic")
+CustomerAddress = pd.read_excel(path, sheet_name="CustomerAddress")
 
-print("Hello World!")
+# This is to check the basic info for each sheet
+# print(NewCustomerList.head(5))
+# print("======================")
+# print(Transactions.head(5))
+# print("======================")
+print(CustomerDemographic.head(5))
+# print("======================")
+# print(CustomerAddress.head(5))
+
+# print(NewCustomerList.info)
